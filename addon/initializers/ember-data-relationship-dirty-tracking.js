09773061929d5d6fb01f375d73c257223e22df4d
Derek Wickern <dwickern@gmail.com>
2024-02-12 13:10:45 -0800

    fix lint errors

--- addon/initializers/ember-data-relationship-dirty-tracking.js
+++ addon/initializers/ember-data-relationship-dirty-tracking.js
@@ -1,6 +1,7 @@
 // imported for side effect
 import 'ember-data-relationship-dirty-tracking/ext';
 
+// eslint-disable-next-line no-unused-vars
 export function initialize(application) {}
 
 export default {
